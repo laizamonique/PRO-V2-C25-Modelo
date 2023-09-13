589f4eaccb5d0666b2ba8a91255b1d0e5334e393
laizamonique <133267385+laizamonique@users.noreply.github.com>
2023-09-13 17:08:24 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -95,19 +95,19 @@ function keyPressed() {
   //   }
   // }
 
-   // if (keyCode === 32) {
-  //   if (numberOfArrows > 0) {
-  //     var posX = playerArcher.body.position.x;
-  //     var posY = playerArcher.body.position.y;
-  //     var angle = playerArcher.body.angle;
+    if (keyCode === 32) {
+    if (numberOfArrows > 0) {
+      var posX = playerArcher.body.position.x;
+     var posY = playerArcher.body.position.y;
+     var angle = playerArcher.body.angle;
 
-  //     var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
+      var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
 
-  //     Matter.Body.setAngle(arrow.body, angle);
-  //     playerArrows.push(arrow);
-  //     numberOfArrows -= 1;
-  //   }
-  // }
+       Matter.Body.setAngle(arrow.body, angle);
+       playerArrows.push(arrow);
+       numberOfArrows -= 1;
+     }
+   }
 
    // if (keyCode === 32) {
   //   if (numberOfArrows > 0) {
